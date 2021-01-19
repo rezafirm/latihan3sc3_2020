d0674bc774ff1458b6ec22b081ed3fe19699e2df
reza <reza@gmail.com>
2021-01-19 21:53:13 +0700

    Fourth Commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 class Hello{
 public static void main(String[] args){
 System.out.println("RMN: Aplikasi Java 2020");
-System.out.println("User 2")
+System.out.println("User 2");
+System.out.println("User 2 Test");
 }
 }
