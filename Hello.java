bea40e7fc7058f7a9dd204806fdc2ad5052da47c
reza <reza@gmail.com>
2021-01-19 21:57:50 +0700

    Fifth Commit

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public static void main(String[] args){
 System.out.println("RMN: Aplikasi Java 2020");
 System.out.println("User 2");
 System.out.println("User 2 Test");
+System.out.println("Reza Reza");
 }
 }
